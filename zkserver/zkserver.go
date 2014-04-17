595f293fd941e6d70dde1e39521dfd76efb70d08
Luka Zakrajšek <luka@bancek.net>
2014-04-17 13:36:31 +0200

    Added zkserver

---
+++ zkserver/zkserver.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"flag"
+	"git.koofr.lan/go-zkutils.git"
+	"log"
+)
+
+func main() {
+	var port = flag.Int("port", 2181, "ZooKeeper port for listening")
+
+	flag.Parse()
+
+	server, err := zkutils.NewTestServer(*port)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer server.Stop()
+
+	log.Printf("ZooKeeper server running on 127.0.0.1:%d", *port)
+
+	<-make(chan int)
+}
